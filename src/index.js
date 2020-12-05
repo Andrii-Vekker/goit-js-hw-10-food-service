b7d4f3d4e291f1dd2d4bf1d34b70637255c368d4
Andrii-Vekker <vekker304@gmail.com>
2020-12-05 23:22:52 +0200

    poka

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // import './scss/common.scss';
 import './styles.css';
 import itemsTemplate from './templates/gallery-items.hbs';
-import menu from './menu.json'
+import menu from './menu.json';
 //console.log(menu)
 
 
 const murkup = itemsTemplate(menu);
-console.log(murkup)
+//console.log(murkup)
 
 const menuRef = document.querySelector('.js-menu');
-console.log(menuRef)
+//console.log(menuRef)
 menuRef.insertAdjacentHTML("beforeend", murkup);
 
 const Theme = {
@@ -17,3 +17,14 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
+const toolbarRef = document.querySelector('.toolbar');
+console.log(toolbarRef)
+const bodyRef = document.querySelector('body');
+console.log(bodyRef);
+const theme_switch_toggle_Ref = document.querySelector('#theme-switch-toggle');
+console.log(theme_switch_toggle_Ref);
+
+
+toolbarRef.addEventListener('change', e => {
+    console.log(e)
+})
